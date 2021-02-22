14866959850986599ba8de31e534701507f9075f
Tom Jemmett <thomas.jemmett@nhs.net>
2021-02-22 17:00:26 +0000

    adds initial target

---
+++ _targets.R
@@ -0,0 +1,22 @@
+library(targets)
+# This is an example _targets.R file. Every
+# {targets} pipeline needs one.
+# Use tar_script() to create _targets.R and tar_edit()
+# to open it again for editing.
+# Then, run tar_make() to run the pipeline
+# and tar_read(summary) to view the results.
+
+# Define custom functions and other global objects.
+# This is where you write source(\"R/functions.R\")
+# if you keep your functions in external scripts.
+
+# Set target-specific options such as packages.
+#tar_option_set(packages = "dplyr")
+
+devtools::document()
+devtools::load_all()
+
+# End this file with a list of target objects.
+list(
+  tar_target(fingertips_data, get_fingertips_data())
+)
