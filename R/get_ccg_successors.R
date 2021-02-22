49d974b1d8c81614c57f9d52a4b5f55f7b6d52e6
Tom Jemmett <thomas.jemmett@nhs.net>
2021-02-22 17:22:21 +0000

    adds ccg successors function and target

---
+++ R/get_ccg_successors.R
@@ -0,0 +1,23 @@
+#' Get CCG Successors
+#'
+#' Maps CCG's from old codes to new codes where a CCG has been succeeded. Also includes a row for each of the current
+#' CCG's from itself to itself
+#'
+#' @return tibble
+#'
+#' @export
+get_ccg_successors <- function() {
+  ccgs <- NHSRtools::ods_get_ccgs() %>%
+    dplyr::filter(stringr::str_ends(name, "CCG"))
+
+  successors <- NHSRtools::ods_get_successors() %>%
+    dplyr::semi_join(ccgs, by = c("old_code" = "org_id")) %>%
+    dplyr::semi_join(ccgs, by = c("new_code" = "org_id")) %>%
+    dplyr::select(old_code, new_code)
+
+  # take all of the ccg's that haven't been succeeded and add them in to list
+  ccgs %>%
+    dplyr::anti_join(successors, by = c("org_id" = "old_code")) %>%
+    dplyr::transmute(old_code = org_id, new_code = org_id) %>%
+    dplyr::bind_rows(successors)
+}
