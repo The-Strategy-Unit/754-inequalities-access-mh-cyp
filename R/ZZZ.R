a92a9c45ddee34a67255b09c2a4f3e0eab984b2e
Tom Jemmett <thomas.jemmett@nhs.net>
2021-02-23 09:31:08 +0000

    solves r cmd check issues

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -1,3 +1,4 @@
 # define any import/importFrom here, see https://kbroman.org/pkg_primer/pages/depends.html
 #' @importFrom dplyr %>%
+#' @importFrom rlang .data .env
 NULL
--- R/get_ccg_successors.R
+++ R/get_ccg_successors.R
@@ -9,17 +9,17 @@
 get_ccg_successors <- function() {
   # load list off ccg's, keep only true ccg's (drops commissioning hubs)
   ccgs <- NHSRtools::ods_get_ccgs() %>%
-    dplyr::filter(stringr::str_ends(name, "CCG"))
+    dplyr::filter(stringr::str_ends(.data$name, "CCG"))
 
   # load ccg successors, make sure to filter both the old and new code columns
   successors <- NHSRtools::ods_get_successors() %>%
     dplyr::semi_join(ccgs, by = c("old_code" = "org_id")) %>%
     dplyr::semi_join(ccgs, by = c("new_code" = "org_id")) %>%
-    dplyr::select(old_code, new_code)
+    dplyr::select(.data$old_code, .data$new_code)
 
   # take all of the ccg's that haven't been succeeded and add them in to list
   ccgs %>%
     dplyr::anti_join(successors, by = c("org_id" = "old_code")) %>%
-    dplyr::transmute(old_code = org_id, new_code = org_id) %>%
+    dplyr::transmute(old_code = .data$org_id, new_code = .data$org_id) %>%
     dplyr::bind_rows(successors)
 }
