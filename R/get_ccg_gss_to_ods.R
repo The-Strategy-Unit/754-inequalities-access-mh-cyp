64297c1710913f6d2c39a7cfe11f4bacfe17dc63
Tom Jemmett <thomas.jemmett@nhs.net>
2021-03-02 13:36:45 +0000

    adds a lookup for ccg codes from GSS codes to ODS codes

---
+++ R/get_ccg_gss_to_ods.R
@@ -0,0 +1,26 @@
+#' Get CCG GSS to ODS code lookup
+#'
+#' Gets the lookup between GSS (E38) and ODS codes
+#'
+#' @return tibble
+#'
+#' @export
+get_ccg_gss_to_ods <- function() {
+  chd_url <- "https://www.arcgis.com/sharing/rest/content/items/2dc879e2df52482ba764d3c11cc3150b/data"
+
+  tmp_dir <- tempdir()
+  chd_zip <- file.path(tmp_dir, "chd.zip")
+  chd_csv <- file.path(tmp_dir, "Equivalents_V2.csv")
+
+  withr::with_file(chd_zip, {
+    download.file(chd_url, chd_zip, mode = "wb")
+    withr::with_file(chd_csv, {
+      unzip(chd_zip, files = "Equivalents_V2.csv", exdir = tmp_dir)
+      chd <- readr::read_csv(chd_csv, col_types = readr::cols(.default = readr::col_character()))
+    })
+  })
+
+  chd %>%
+    dplyr::filter(ENTITYCD == "E38") %>%
+    dplyr::distinct(ccgcd = GEOGCD, ccgcdh = GEOGCDH)
+}
