c7611ead26dd803d5ab2b9f0c8d9f51de25ebd3c
Tom Jemmett <thomas.jemmett@nhs.net>
2021-04-21 19:25:03 +0100

    sorts targets out to load the package properly

--- _targets.R
+++ _targets.R
@@ -1,20 +1,15 @@
 library(targets)
-# This is an example _targets.R file. Every
-# {targets} pipeline needs one.
-# Use tar_script() to create _targets.R and tar_edit()
-# to open it again for editing.
-# Then, run tar_make() to run the pipeline
-# and tar_read(summary) to view the results.
+library(InequalitiesAccessCYPMH)
 
 # Define custom functions and other global objects.
 # This is where you write source(\"R/functions.R\")
 # if you keep your functions in external scripts.
 
 # Set target-specific options such as packages.
-#tar_option_set(packages = "dplyr")
-
-devtools::document()
-devtools::load_all()
+tar_option_set(
+  packages = c("InequalitiesAccessCYPMH"),
+  imports = c("InequalitiesAccessCYPMH")
+)
 
 check_for_issues()
 
