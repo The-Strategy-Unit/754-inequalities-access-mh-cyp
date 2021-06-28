c60e98ab895892cef6f370510eb3e69f88210cb0
Tom Jemmett <tom.jemmett@gmail.com>
2021-06-28 11:02:28 +0100

    adds mlp functions

---
+++ R/cypmh_model_run_mlp.R
@@ -0,0 +1,50 @@
+#' CYPMH Model - MLP
+#'
+#' Runs a neural network MLP model on the CYPMH data
+#'
+#' @param cypmh the cypmh data
+#'
+#' @return the model
+#'
+#' @export
+cypmh_model_run_mlp <- function(cypmh) {
+  u <- levels(cypmh$util_description)
+
+  x_train <- cypmh %>%
+    dplyr::select(-.data$util_description) %>%
+    as.matrix()
+  y_train <- keras::to_categorical(as.numeric(cypmh$util_description) - 1, length(u))
+  colnames(y_train) <- u
+
+  x_val <- x_train[1:10000,]
+  y_val <- y_train[1:10000,]
+
+  x_train <- x_train[-(1:10000),]
+  y_train <- y_train[-(1:10000),]
+
+  m <- keras::keras_model_sequential() %>%
+    keras::layer_dense(units = 64,
+                       activation = "relu",
+                       input_shape = c(ncol(cypmh) - 1)) %>%
+    keras::layer_dropout(0.3) %>%
+    keras::layer_dense(units = 32, activation = "relu") %>%
+    keras::layer_dropout(0.5) %>%
+    keras::layer_dense(units = 16, activation = "relu") %>%
+    keras::layer_dense(units = length(u), activation = "softmax")
+
+  m %>% keras::compile(
+    optimizer = "rmsprop",
+    loss = "binary_crossentropy",
+    metrics = c("accuracy")
+  )
+
+  m %>% keras::fit(
+    x_train,
+    y_train,
+    epochs = 8,
+    batch_size = 512,
+    validation_data = list(x_val, y_val)
+  )
+
+  m
+}
